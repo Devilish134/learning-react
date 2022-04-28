72f3f0e15f6f64f4f76730a1a9e63ed02240e2fd
Devilish134 <kamilmusialik22@gmail.com>
2022-04-28 11:11:54 +0200

    + lists in storage

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -28,7 +28,6 @@ const NavBar = () => {
             {name}
           </NavLink>
         ))}
-        ;
       </div>
     </nav>
   );
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,18 +1,40 @@
 const initialState = {
+  lists: [
+    {
+      id: 1,
+      title: 'Things to do...',
+      description:
+        'Interesting things I want to check out',
+    },
+    {
+      id: 2,
+      title: 'Test list',
+      description: 'Lorem Ipsum',
+    },
+  ],
   columns: [
     {
       id: 1,
+      listId: 1,
       title: 'Books',
       icon: 'book',
     },
     {
       id: 2,
+      listId: 1,
       title: 'Movies',
       icon: 'film',
     },
     {
       id: 3,
+      listId: 1,
       title: 'Games',
+      icon: 'gamepad',
+    },
+    {
+      id: 4,
+      listId: 2,
+      title: 'Test',
       icon: 'gamepad',
     },
   ],
@@ -40,6 +62,7 @@ const initialState = {
       title: 'The Witcher',
     },
     { id: 6, columnId: 3, title: 'Skyrim' },
+    { id: 7, columnId: 4, title: 'test' },
   ],
 
   search: '',
