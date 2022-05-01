ffddbb13ae383fd03a936ffafd2403f9d6076ad5
Devilish134 <kamilmusialik22@gmail.com>
2022-05-01 15:45:16 +0200

    + clsx, favorite styles & favorite icon

--- src/components/Card/Card..js
+++ src/components/Card/Card..js
@@ -1,8 +1,14 @@
 import styles from './Card.module.scss';
+import clsx from 'clsx';
 
 const Card = ({ title }) => {
   return (
-    <li className={styles.card}>{title}</li>
+    <li className={styles.card}>
+      {title}
+      <span
+        className={clsx('  fa fa-star-o')}
+      />
+    </li>
   );
 };
 
--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,10 +1,20 @@
+import Card from '../Card/Card.';
+import styles from './Favorite.module.scss';
 import PageTitle from '../PageTitle/PageTitle';
 
-const Favorite = () => {
+const Favorite = (card, id, action) => {
   return (
     <div>
       <PageTitle>Favorite</PageTitle>
       <p>Here are Your Favorite titles</p>
+      <article className={styles.column}>
+        <ul className={styles.cards}>
+          <Card
+            key={card.id}
+            title={card.title}
+          />
+        </ul>
+      </article>
     </div>
   );
 };
